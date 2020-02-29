84db91cca52ea33c3efddd30a0f64a626fcd6a28
liushen1 <liushen1@vipkid.com.cn>
2020-02-29 23:18:54 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  let [list, setList] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get('http://server.jsliushen.com/demo/users/list')
+      if (res && res.data && res.data.code === 200 && Array.isArray(res.data.data)) {
+        setList(res.data.data)
+      }
+    })()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {list.map(item => (
+        <li key={item.id}>{item.userName}-{item.age}</li>
+      ))}
+    </ul>
+  )
 }
 
 export default App;
